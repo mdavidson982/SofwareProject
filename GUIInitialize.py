c932725a848f6a8f91143f4beb2df6886982b5f4
Ahart36 <ahartman781@g.rwu.edu>
2023-04-14 14:49:13 -0400

    Fixed Input box
    File path is now back into entry box. must pack widgets after function definitions to use widget functions

--- GUIInitialize.py
+++ GUIInitialize.py
@@ -14,42 +14,42 @@ import os
 import FileReading as fr
 import DataProcessing as dp
 
-class Window():
-    
-    def __init__(self,master):
-            frame0 = Frame(master)
-            label= Label(frame0, text="test").pack()
-            frame0.pack(side=LEFT)
-
-            frame1 = Frame(master)
-            r = st.ScrolledText(frame1, width = 30, height = 10,font = ("Times New Roman", 12)).pack()
-            frame1.pack(padx=10,pady=10, side= RIGHT)
-            
-            frame2 = Frame(master)
-            lb= Label(frame2, text = "Enter filepath").pack(side=LEFT)
-            e =Entry(frame2).pack(side=LEFT)
-            calc = Button(frame2, text = "Enter").pack(side=RIGHT)
-            browse = Button(frame2, text = "Browse", command = lambda:browse_click()).pack(side=LEFT)
-            frame2.pack(padx=10,pady=10, side=BOTTOM)
-
-            def browse_click():
-                file = askopenfile(mode = 'r' , filetypes= [('RUN Files','*.RUN')]) #opens file browser system
-                if file is not NONE: 
-                    filepath = os.getcwd()
-                    e.insert(0,filepath)  #gets current working directory and pastes in to the input box
-
-            def button_enter():
-                e.get()
-            
-                            #MUST USE DIRECTORY FUNCTIONS CWD
-
-            def text_box():
-                r.insert("Data") #CALL DATA PROCESSING FROM FILE READING
-
-
-
 window = tk.Tk()
-root = Window(window)
+    
+frame0 = Frame(window)
+label= Label(frame0, text="test").pack()
+frame0.pack()
+
+frame1 = Frame(window)
+r = st.ScrolledText(frame1, width = 30, height = 10,font = ("Times New Roman", 12))
+frame1.pack(padx=10,pady=10)
+            
+frame2 = Frame(window)
+lb = Label(frame2, text = "Enter filepath").pack(side=LEFT)
+e = Entry(frame2)
+calc = Button(frame2, text = "Enter")
+browse = Button(frame2, text = "Browse", command=lambda:browse_click()).pack(side=LEFT)
+frame2.pack(padx=10,pady=10)
+
+def browse_click():
+    file = askopenfile(mode = 'r' , filetypes= [('RUN Files','*.RUN')]) #opens file browser system
+    if file is not NONE: 
+        filepath = os.getcwd()
+        e.insert(0,filepath)
+                  #gets current working directory and pastes in to the input box
+
+def button_enter():
+    e.get()
+            #File Reading functions
+                            
+
+def text_box():
+    r.insert("Data") #CALL DATA PROCESSING FROM FILE READING
+
+e.pack()
+r.pack()
+calc.pack()
+
 window.title("GPA Calculator")
 
 window.mainloop()
