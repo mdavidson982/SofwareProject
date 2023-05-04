5ba55119181624f18a20b267998ffae67c1d51d3
Matthew Davidson <mdavidson982@g.rwu.edu>
2023-05-04 12:42:11 -0400

    Files Open With dirrectory not executable location

--- FileReading.py
+++ FileReading.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 #Developed By Matthew Davidson
 #This class reads the RUN file and creates a dataframe that consist of all the students.
 
@@ -18,6 +19,7 @@ class FileReading:
   def __init__(self,runFile):
     #self.runFile is basically saying this.runFile in Java.
     self.runFile = runFile
+    self.directory = os.path.dirname(self.runFile)
     #this is the dataFrame that will be returned in the openFile function
     #Its an empty dataframe that has 6 columns. 
     self.sectionData = pd.DataFrame(columns=["Group","Section","First Name","Last Name","Student ID", "Grade"])
@@ -45,17 +47,20 @@ class FileReading:
     #a 2d array where every row index represents a new group and every colum index represents an SEC file.
     #The first index of every row is the group associated with the sec files.
     group2dArray = []
-    for i in range(len(runFileArray)):
     
+    for i in range(len(runFileArray)):
+      
       #Must be when i is greater the 0, because the first index of the runFileArray is the name of the runfile.
       if i > 0:
+        self.directory = os.path.dirname(self.runFile)
+        self.directory = self.directory +"/"+ runFileArray[i]
         #debugging lines. Prints the current group being added to the group2dArray
         #grp = "Opening {group} file".format(group = runFileArray[i])
         #print(grp)
 
         #opens the current group file
         try:
-          with open(runFileArray[i], 'r') as group:
+          with open(self.directory, 'r') as group:
             secFiles = []
             #For everyline in the groupfile we list the sec files that need to be opened and appened them to secFiles
             for line in group:
@@ -63,7 +68,7 @@ class FileReading:
 
             group2dArray.append(secFiles)
         except FileNotFoundError as e:
-          return e,runFileArray[i]
+          return e,self.directory
     #prints the list of all sec files for each group. Look at the initialization of group2dArray to better understand it's content.
     #print(group2dArray)
 
@@ -77,9 +82,13 @@ class FileReading:
       #Loop through each COLUMN for each ROW in group2dArray. Except when J = 0 (because that would be the group name). This gets the file name for each SEC file included in RUN
       for j in range(len(group2dArray[i])):
         if j > 0:
+          self.directory = os.path.dirname(self.runFile)
+          self.directory = self.directory +"/"+ group2dArray[i][j]
+          #print(group2dArray)
+          #print(self.directory)
           #open the SEC file. Read only. Name that opened file 'section'
           try:
-            with open(group2dArray[i][j], 'r') as section:
+            with open(self.directory, 'r') as section:
               #section includeds four values when seperated by the delimiter ",". This would be the first name, last name, student id, and grade of each student in the class.
               #classData is a 2d array where each row represents a student and each column for each row is data about the student.
               classData = np.genfromtxt(section,skip_header=1,delimiter=",", dtype = str)
@@ -98,6 +107,6 @@ class FileReading:
                   
                   self.sectionData = pd.concat([self.sectionData, tempFrame], ignore_index=True)
           except FileNotFoundError as e:
-            return e,group2dArray[i][j]
+            return e,self.directory
     #print(self.sectionData)
     return self.sectionData
